7f14ae301c7229000a0b6572febbf1d9ad91278e
root <ngomfa@gmail.com>
2018-11-16 15:50:38 +0100

    exo5

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Partie1_Exo5</title>
+</head>
+<body>
+  <?php
+//exo: 1-Créer une variable de type int. L'initialiser avec rien. Afficher sa valeur.
+//     2-Donner une valeur à cette variable et l'afficher.
+    $age = null;
+    settype($age, "integer");
+    echo $age;
+//settype — Affecte un type à une variable
+
+  ?>
+</body>
+</html>
